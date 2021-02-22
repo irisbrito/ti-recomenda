54106c5096ee7402917a7711302d2b914f20304c
Iris <iris.souza@zup.com.br>
2021-02-22 17:47:48 -0300

    Adicionar classe filme

---
+++ src/com/br/zup/Filme.java
@@ -0,0 +1,42 @@
+package com.br.zup;
+
+import java.util.List;
+import java.util.Map;
+
+public class Filme {
+    private String titulo;
+    private String genero;
+    private List <String> emailDeQuemIndicou;
+    private String email;
+
+    public Filme(String titulo, String genero, String emailDeQuemIndicou) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.email = email;
+        //this.emailDeQuemIndicou = emailDeQuemIndicou;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public List<String> getEmailDeQuemIndicou() {
+        return emailDeQuemIndicou;
+    }
+
+    public void setEmailDeQuemIndicou(List<String> emailDeQuemIndicou) {
+        this.emailDeQuemIndicou = emailDeQuemIndicou;
+    }
+}
