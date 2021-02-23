96482b381a9f6f60f8dbbddfbf6dc0db6d3a6237
Iris <iris.souza@zup.com.br>
2021-02-23 17:46:42 -0300

    Adicionar opção deletar usuário no menu

--- src/com/br/zup/Sistema.java
+++ src/com/br/zup/Sistema.java
@@ -24,9 +24,10 @@ public class Sistema {
         IO.mostrar("1. Adicionar uma recomendação");
         IO.mostrar("2. Apagar uma recomendação");
         IO.mostrar("3. Criar um usuário");
-        IO.mostrar("4. Adicionar aos seus favoritos");
-        IO.mostrar("5. Visualizar as recomendações cadastradas");
-        IO.mostrar("6. Sair:");
+        IO.mostrar("4. Deletar um usuário");
+        IO.mostrar("5. Adicionar aos seus favoritos");
+        IO.mostrar("6. Visualizar as recomendações cadastradas");
+        IO.mostrar("7. Sair:");
 
     }
 
@@ -146,10 +147,12 @@ public class Sistema {
         } else if (opcao == 3) {
             adicionarUsuario();
         } else if (opcao == 4) {
-            adicionarAosSeusFavoritos();
+            deletarUsuario();
         } else if (opcao == 5) {
-            visualizarRecomendacoes();
+            adicionarAosSeusFavoritos();
         } else if (opcao == 6) {
+            visualizarRecomendacoes();
+        } else if (opcao == 7) {
             continuaExecutando = false;
         }
     }
