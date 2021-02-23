61855836884614b03775682e0c66f71ea8e6786f
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-23 15:46:42 -0300

    Adicionar documentação da classe IO

--- src/com/br/zup/IO.java
+++ src/com/br/zup/IO.java
@@ -2,19 +2,43 @@ package com.br.zup;
 
 import java.util.Scanner;
 
+/**
+ * Classe que controla mostrar texto na tela e pegar dados do usuário
+ *
+ */
 public class IO {
+    /**
+     * Pega uma linha qe o usuário digitou do console
+     *
+     * @return uma string com o que o usuário digitou
+     */
     public static String pegarLinha() {
         return new Scanner(System.in).nextLine();
     }
 
+    /**
+     * Pega um inteiro digitado pelo usuário
+     *
+     * @return um int contendo o número que o usuário digitou
+     */
     public static int pegarInt() {
         return new Scanner(System.in).nextInt();
     }
 
+    /**
+     * * Pega um double digitado pelo usuário
+     *
+     * @return um double contendo o número que o usuário digitou
+     */
     public static double pegarDouble() {
         return new Scanner(System.in).nextDouble();
     }
 
+    /**
+     * Mostra uma mensagem na tela
+     *
+     * @param mensagem uma string com a mensagem que deve ser mostrada na tela
+     */
     public static void mostrar(String mensagem) {
         System.out.println(mensagem);
     }
