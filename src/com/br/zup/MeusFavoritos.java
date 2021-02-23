c9ef292e7b5aecaf0297b102ba4fb0e34db9d701
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-02-23 16:20:40 -0300

    Inserindo documentação na classe MeusFavoritos

--- src/com/br/zup/MeusFavoritos.java
+++ src/com/br/zup/MeusFavoritos.java
@@ -2,6 +2,9 @@ package com.br.zup;
 
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * Classe para criar a lista de filmes favoritos para cada usuario
+ */
 
 public class MeusFavoritos extends Catalogo {
     public MeusFavoritos() {
