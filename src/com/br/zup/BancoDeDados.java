61075ece2cc73ba45ea1ec9ac8236ececd17b6f4
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-25 14:18:21 -0300

    Terminar classe banco de dados. Foi necessário adicionar um getEmail na classe filme para poder completar

--- src/com/br/zup/BancoDeDados.java
+++ src/com/br/zup/BancoDeDados.java
@@ -1,6 +1,7 @@
 package com.br.zup;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class BancoDeDados {
@@ -45,6 +46,36 @@ public class BancoDeDados {
             catalogo.adicionarFilme(filme.getTitulo(), filme.getGenero(), filme.getEmail());
         }
 
+        leitor.close();
         return catalogo;
+    }
+
+    private static void salvarUsuarioParaArquivo(FileWriter escritor, Usuario usuario) throws Exception {
+        escritor.write(usuario.getNome() + "\n"); // \n para ele pular para a linha seguinte
+        escritor.write(usuario.getEmail() + "\n");
+    }
+
+    public static void salvarListaDeUsuariosParaArquivo(String nomeDoArquivo, ListaDeUsuarios listaDeUsuarios)throws Exception  {
+        FileWriter escritor = new FileWriter(nomeDoArquivo);
+
+        for (Usuario usuario: listaDeUsuarios.getUsuarios()) {
+            salvarUsuarioParaArquivo(escritor, usuario);
+        }
+    }
+
+    private static void salvarFilmeParaArquivo(FileWriter escritor, Filme filme)throws Exception  {
+        escritor.write(filme.getTitulo());
+        escritor.write(filme.getGenero());
+        escritor.write(filme.getEmail());
+    }
+
+    public static void salvarCatalogoParaArquivo(String nomeDoArquivo, Catalogo catalogo) throws Exception {
+        FileWriter escritor = new FileWriter(nomeDoArquivo);
+
+        for (Filme filme: catalogo.getFilmes()) {
+            salvarFilmeParaArquivo(escritor, filme);
+        }
+
+        escritor.close();
     }
 }
