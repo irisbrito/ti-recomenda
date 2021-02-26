460c467f75546c83b4a62f8c96a2361f76315ce7
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-02-26 13:35:04 -0300

    Adicionar documentação na classe ListaDeUsuarios

--- src/com/br/zup/ListaDeUsuarios.java
+++ src/com/br/zup/ListaDeUsuarios.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class ListaDeUsuarios {
     private List<Usuario> usuarios;
+/**
+ * A classe ListadeUsuários é uma lista composta com todos os usuários cadastrados na plataforma
+ */
 
     public ListaDeUsuarios() {
         usuarios = new ArrayList<>();
     }
-
+/**
+ *  O método adicionarUsuario permite que um novo usuário seja cadastrado no sistema
+ */
     public Usuario adicionarUsuario(String nome, String email)throws Exception  {
         try {
             Usuario usuario = pesquisarUsuarioPeloEmail(email);
@@ -22,7 +27,10 @@ public class ListaDeUsuarios {
 
         throw new Exception("Usuário já está na lista.");
     }
-
+    /**
+     *  O método pesquisarUsuarioPeloEmail permite a busca por um usuário que já esteja
+     *  cadastrado no sistema utilizando o email
+     */
     public Usuario pesquisarUsuarioPeloEmail(String email) throws Exception {
         for (Usuario usuario : usuarios) {
             if ( usuario.getEmail().equalsIgnoreCase(email) ) {
@@ -32,13 +40,18 @@ public class ListaDeUsuarios {
 
         throw new Exception("Usuário não encontrado");
     }
-
+    /**
+     *  O método removerUsuarioPeloEmail permite excluir da base de dados um usuário que já esteja
+     *  cadastrado no sistema
+     */
     public void removerUsuarioPeloEmail(String email) throws Exception {
         Usuario usuario = pesquisarUsuarioPeloEmail(email);
 
         usuarios.remove(usuario);
         }
-
+    /**
+     *  O método isEmailvalido verifica se o email informado pelo usuário é um email válido
+     */
     public static boolean isEmailvalido( String email) throws Exception {
         if (!email.contains("@") && !email.contains(".com")) {
             throw new Exception("E-mail invalido");
@@ -46,7 +59,10 @@ public class ListaDeUsuarios {
             return true;
         }
     }
-
+    /**
+     *  O método verificarSeEmailEstaCadastrado verifica não permite
+     *  que mais de um usuário utilize o mesmo email
+     */
     public void verificarSeEmailEstaCadastrado(String email) throws Exception {
         for(Usuario usuario: usuarios){
             if(usuario.getEmail().equals(email)){
